4435686e6faee148b09ea845c5c54a3b37c65864
dts1234 <adas.kazmierczak@gmail.com>
2020-10-08 19:14:25 +0200

    part_1 commit

--- src/contactApp/Controller.java
+++ src/contactApp/Controller.java
@@ -1,4 +1,16 @@
 package contactApp;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
 public class Controller {
+
+    @FXML
+    public void showAddContactDialog(){
+
+        FXMLLoader loader = new FXMLLoader();
+
+
+    }
+
 }
